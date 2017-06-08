3bbff6c7506ed5dc60d41a7f88ebec064d3a2255
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-08 11:27:25 -0400

    use multiple modifiers w/ directive

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Directives</h1>
         <p v-highlight:background.delay="'lightblue'">Color this</p>
-        <p v-local-highlight:background.delay="'lightblue'">Color this too</p>
+        <p v-local-highlight:background.delay.blink="'lightblue'">Color this too</p>
       </div>
     </div>
   </div>
@@ -27,14 +27,32 @@
           if (binding.modifiers['delay']) {
             delay = 3000;
           }
-          setTimeout(() => {
-            if (binding.arg == 'background') {
-              el.style.backgroundColor = binding.value;
-            }
-            else {
-              el.style.color = binding.value;
-            }
-          }, delay);
+          if (binding.modifiers['blink']) {
+            let mainColor = binding.value;
+            let secondColor = 'blue';
+            let currentColor = mainColor;
+            setTimeout(() => {
+              setInterval(() => {
+                currentColor === secondColor ? currentColor = mainColor : currentColor = secondColor;
+                if (binding.arg == 'background') {
+                  el.style.backgroundColor = currentColor;
+                }
+                else {
+                  el.style.color = currentColor;
+                }
+              }, 1000);
+            }, delay)
+          }
+          else {
+            setTimeout(() => {
+              if (binding.arg == 'background') {
+                el.style.backgroundColor = binding.value;
+              }
+              else {
+                el.style.color = binding.value;
+              }
+            }, delay);
+          }
         }
       }
     }
