9c82dd64c02c3bc7228a62d39c0246c4c786872a
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-08 03:43:04 -0400

    modify custom directive with modifier

--- src/main.js
+++ src/main.js
@@ -3,14 +3,20 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
+    let delay = 0;
+    if (binding.modifiers['delay']) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      }
+      else {
+        el.style.color = binding.value;
+      }
+    }, delay);
     // el.style.backgroundColor = 'green';
     // el.style.backgroundColor = binding.value;
-    if (binding.arg == 'background') {
-      el.style.backgroundColor = binding.value;
-    }
-    else {
-      el.style.color = binding.value;
-    }
   }
 });
 
